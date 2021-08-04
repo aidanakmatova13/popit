7c3954595cd8a7351e6098fe30b27f8d2ca6bbd9
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-04 19:21:09 +0300

    fix code

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
-const wrapper = document.querySelector('.wrapper')
 const divs = document.querySelectorAll('.wrapper div')
+const colors = ['dark', 'red', 'green', 'blue', 'orange', 'yellow', 'purple']
+const wrapper = document.querySelector('.wrapper')
 
 
 
-
-divs.forEach(div =>{
-    div.addEventListener("click", (e)=>{
-        e.target.classList.toggle('black')
+divs.forEach((div,idx)=>{
+    div.addEventListener('click',() =>{
+        const random = Math.floor(Math.random()*7)
+        divs.forEach(div =>div.removeAttribute('class'))
+        if (idx === divs.length -1){
+            idx = -1
+        }
+        divs[idx+1].classList.add(colors[random])
     })
 })
 
 
-// divs.forEach(div =>{
-//     div.target.classList.remove('black')
+
+//этот код по структуре правильнее
+// wrapper.addEventListener('click', e =>{
+//     if (e.target.classList[0] !== 'wrapper'){
+//         const random = Math.floor(Math.random()*7)
+//         divs.forEach(div =>div.removeAttribute('class'))
+//         e.target.classList.add(colors[random])
+//     }
 // })
-
-
-// divs.forEach(div =>{
-//     div.classList.remove('black')
-//     div.addEventListener("click", (e)=>{
-//         e.target.classList.add('black')
-//     })
-// })
-
-
-// function click(){
-//     divs.forEach(div =>{
-//         div.classList.remove('black')
-//         div.addEventListener("click", (e)=>{
-//             e.target.classList.add('black')
-//         })
-//     })
-// }
 
